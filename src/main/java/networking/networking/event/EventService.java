f344c3860a4e17d00af7a31a34d973718f07cd80
Gyokhan <gyokhan28@gmail.com>
2024-04-20 10:21:47 +0300

    Added form to add new events

--- src/main/java/networking/networking/event/EventService.java
+++ src/main/java/networking/networking/event/EventService.java
@@ -1,7 +1,25 @@
 package networking.networking.event;
 
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 @Service
 public class EventService {
+
+    EventRepository eventRepository;
+
+    public String showAddEventForm(Model model){
+        model.addAttribute("event", new Event());
+        return "event-register";
+    }
+
+    public String saveEvent(Event event, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("event", event);
+            return "event-register";
+        }
+        eventRepository.save(event);
+        return "event-register";
+    }
 }
